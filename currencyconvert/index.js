aff8c52350ceccc4209df8b862eeb8bdef06dfe6
Hemant Dixit <hemantdixit00000@gmail.com>
2025-12-23 00:38:50 +0530

    New Functionality added inside exchange

--- currencyconvert/index.js
+++ currencyconvert/index.js
@@ -1,240 +1,107 @@
 const countryList = {
-    AED: "AE",
-    AFN: "AF",
-    XCD: "AG",
-    ALL: "AL",
-    AMD: "AM",
-    ANG: "AN",
-    AOA: "AO",
-    AQD: "AQ",
-    ARS: "AR",
-    AUD: "AU",
-    AZN: "AZ",
-    BAM: "BA",
-    BBD: "BB",
-    BDT: "BD",
-    XOF: "BE",
-    BGN: "BG",
-    BHD: "BH",
-    BIF: "BI",
-    BMD: "BM",
-    BND: "BN",
-    BOB: "BO",
-    BRL: "BR",
-    BSD: "BS",
-    NOK: "BV",
-    BWP: "BW",
-    BYR: "BY",
-    BZD: "BZ",
-    CAD: "CA",
-    CDF: "CD",
-    XAF: "CF",
-    CHF: "CH",
-    CLP: "CL",
-    CNY: "CN",
-    COP: "CO",
-    CRC: "CR",
-    CUP: "CU",
-    CVE: "CV",
-    CYP: "CY",
-    CZK: "CZ",
-    DJF: "DJ",
-    DKK: "DK",
-    DOP: "DO",
-    DZD: "DZ",
-    ECS: "EC",
-    EEK: "EE",
-    EGP: "EG",
-    ETB: "ET",
-    EUR: "FR",
-    FJD: "FJ",
-    FKP: "FK",
-    GBP: "GB",
-    GEL: "GE",
-    GGP: "GG",
-    GHS: "GH",
-    GIP: "GI",
-    GMD: "GM",
-    GNF: "GN",
-    GTQ: "GT",
-    GYD: "GY",
-    HKD: "HK",
-    HNL: "HN",
-    HRK: "HR",
-    HTG: "HT",
-    HUF: "HU",
-    IDR: "ID",
-    ILS: "IL",
-    INR: "IN",
-    IQD: "IQ",
-    IRR: "IR",
-    ISK: "IS",
-    JMD: "JM",
-    JOD: "JO",
-    JPY: "JP",
-    KES: "KE",
-    KGS: "KG",
-    KHR: "KH",
-    KMF: "KM",
-    KPW: "KP",
-    KRW: "KR",
-    KWD: "KW",
-    KYD: "KY",
-    KZT: "KZ",
-    LAK: "LA",
-    LBP: "LB",
-    LKR: "LK",
-    LRD: "LR",
-    LSL: "LS",
-    LTL: "LT",
-    LVL: "LV",
-    LYD: "LY",
-    MAD: "MA",
-    MDL: "MD",
-    MGA: "MG",
-    MKD: "MK",
-    MMK: "MM",
-    MNT: "MN",
-    MOP: "MO",
-    MRO: "MR",
-    MTL: "MT",
-    MUR: "MU",
-    MVR: "MV",
-    MWK: "MW",
-    MXN: "MX",
-    MYR: "MY",
-    MZN: "MZ",
-    NAD: "NA",
-    XPF: "NC",
-    NGN: "NG",
-    NIO: "NI",
-    NPR: "NP",
-    NZD: "NZ",
-    OMR: "OM",
-    PAB: "PA",
-    PEN: "PE",
-    PGK: "PG",
-    PHP: "PH",
-    PKR: "PK",
-    PLN: "PL",
-    PYG: "PY",
-    QAR: "QA",
-    RON: "RO",
-    RSD: "RS",
-    RUB: "RU",
-    RWF: "RW",
-    SAR: "SA",
-    SBD: "SB",
-    SCR: "SC",
-    SDG: "SD",
-    SEK: "SE",
-    SGD: "SG",
-    SKK: "SK",
-    SLL: "SL",
-    SOS: "SO",
-    SRD: "SR",
-    STD: "ST",
-    SVC: "SV",
-    SYP: "SY",
-    SZL: "SZ",
-    THB: "TH",
-    TJS: "TJ",
-    TMT: "TM",
-    TND: "TN",
-    TOP: "TO",
-    TRY: "TR",
-    TTD: "TT",
-    TWD: "TW",
-    TZS: "TZ",
-    UAH: "UA",
-    UGX: "UG",
-    USD: "US",
-    UYU: "UY",
-    UZS: "UZ",
-    VEF: "VE",
-    VND: "VN",
-    VUV: "VU",
-    YER: "YE",
-    ZAR: "ZA",
-    ZMK: "ZM",
-    ZWD: "ZW",
-  };
+    AED: "AE", AFN: "AF", XCD: "AG", ALL: "AL", AMD: "AM",
+    ANG: "AN", AOA: "AO", AQD: "AQ", ARS: "AR", AUD: "AU",
+    AZN: "AZ", BAM: "BA", BBD: "BB", BDT: "BD", XOF: "BE",
+    BGN: "BG", BHD: "BH", BIF: "BI", BMD: "BM", BND: "BN",
+    BOB: "BO", BRL: "BR", BSD: "BS", NOK: "BV", BWP: "BW",
+    BYR: "BY", BZD: "BZ", CAD: "CA", CDF: "CD", XAF: "CF",
+    CHF: "CH", CLP: "CL", CNY: "CN", COP: "CO", CRC: "CR",
+    CUP: "CU", CVE: "CV", CYP: "CY", CZK: "CZ", DJF: "DJ",
+    DKK: "DK", DOP: "DO", DZD: "DZ", ECS: "EC", EEK: "EE",
+    EGP: "EG", ETB: "ET", EUR: "FR", FJD: "FJ", FKP: "FK",
+    GBP: "GB", GEL: "GE", GGP: "GG", GHS: "GH", GIP: "GI",
+    GMD: "GM", GNF: "GN", GTQ: "GT", GYD: "GY", HKD: "HK",
+    HNL: "HN", HRK: "HR", HTG: "HT", HUF: "HU", IDR: "ID",
+    ILS: "IL", INR: "IN", IQD: "IQ", IRR: "IR", ISK: "IS",
+    JMD: "JM", JOD: "JO", JPY: "JP", KES: "KE", KGS: "KG",
+    KHR: "KH", KMF: "KM", KPW: "KP", KRW: "KR", KWD: "KW",
+    KYD: "KY", KZT: "KZ", LAK: "LA", LBP: "LB", LKR: "LK",
+    LRD: "LR", LSL: "LS", LTL: "LT", LVL: "LV", LYD: "LY",
+    MAD: "MA", MDL: "MD", MGA: "MG", MKD: "MK", MMK: "MM",
+    MNT: "MN", MOP: "MO", MRO: "MR", MTL: "MT", MUR: "MU",
+    MVR: "MV", MWK: "MW", MXN: "MX", MYR: "MY", MZN: "MZ",
+    NAD: "NA", XPF: "NC", NGN: "NG", NIO: "NI", NPR: "NP",
+    NZD: "NZ", OMR: "OM", PAB: "PA", PEN: "PE", PGK: "PG",
+    PHP: "PH", PKR: "PK", PLN: "PL", PYG: "PY", QAR: "QA",
+    RON: "RO", RSD: "RS", RUB: "RU", RWF: "RW", SAR: "SA",
+    SBD: "SB", SCR: "SC", SDG: "SD", SEK: "SE", SGD: "SG",
+    SKK: "SK", SLL: "SL", SOS: "SO", SRD: "SR", STD: "ST",
+    SVC: "SV", SYP: "SY", SZL: "SZ", THB: "TH", TJS: "TJ",
+    TMT: "TM", TND: "TN", TOP: "TO", TRY: "TR", TTD: "TT",
+    TWD: "TW", TZS: "TZ", UAH: "UA", UGX: "UG", USD: "US",
+    UYU: "UY", UZS: "UZ", VEF: "VE", VND: "VN", VUV: "VU",
+    YER: "YE", ZAR: "ZA", ZMK: "ZM", ZWD: "ZW",
+};
 
+const selects = document.querySelectorAll(".allbox select");
+const imgs = document.querySelectorAll(".allbox img");
+const btn = document.querySelector(".btnn");
+const exchange = document.getElementById("Exchange");
+const input = document.querySelector(".ammount");
+const result = document.querySelector(".value");
 
+const API_URL = "https://api.exchangerate-api.com/v4/latest";
 
-let allImg = document.querySelectorAll(".allbox img")
-let from = document.querySelector("#from select")
+// 🔹 Populate dropdowns
+for (let select of selects) {
+    for (let code in countryList) {
+        let option = document.createElement("option");
+        option.value = code;
+        option.innerText = code;
 
-let all = document.querySelectorAll(".allbox select")
-let url ="https://api.exchangerate-api.com/v4/latest"
+        if (select.name === "from" && code === "USD") option.selected = true;
+        if (select.name === "to" && code === "INR") option.selected = true;
 
-for(let select of all){
-    for(code in countryList){
-        let newOption = document.createElement("option")
-        newOption.innerText=`${code}  TO ${countryList[code]}`
-        newOption.setAttribute("class",`${code}`)
-        newOption.classList.add(code)
-        newOption.value=code
-        if(select.name==="from" && code==="USD"){
-            newOption.selected="selected"
-        }else if(select.name==="to" && code==="INR"){
-            newOption.selected="selected"
-        }
-        select.appendChild(newOption)
+        select.appendChild(option);
     }
-    select.addEventListener("change",(e)=>{
-        cunCode(e.target)
-    })
+
+    select.addEventListener("change", () => updateFlag(select));
 }
 
-let cunCode =(eve)=>{
-    let curCode =eve.value
-    let cunCode =countryList[curCode]
-    let newSrc =`https://flagsapi.com/${cunCode}/flat/64.png`
-    let img = eve.parentElement.querySelector("img")
-    img.src=newSrc
+// 🔹 Update flag
+function updateFlag(select) {
+    let countryCode = countryList[select.value];
+    let img = select.parentElement.querySelector("img");
+    img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
 }
 
-let button = document.querySelector(".btnn")
-let getData=async()=>{
-    n = `${url}/${all[0].value}`
-    da =  await fetch(n)
-    respo = await da.json()
-    frm = respo.rates[from]
-    rateAmount=respo.rates[getRate]
-    val.innerText=`${amm} ${fro} IN ${getRate} = ${rateAmount*amm}`
+// 🔹 Get exchange rate
+async function getRate() {
+    let amount = Number(input.value);
+    if (!amount || amount <= 0) {
+        amount = 1;
+        input.value = 1;
+    }
+
+    let from = selects[0].value;
+    let to = selects[1].value;
+
+    let response = await fetch(`${API_URL}/${from}`);
+    let data = await response.json();
+
+    let rate = data.rates[to];
+    result.innerText = `${amount} ${from} To ${to} = ${(rate * amount).toFixed(2)}`;
 }
-let da
-let respo
-let getRate=all[1].value
-let fro=all[0].value
-let val=document.querySelector(".value")
-let n
-let data =document.querySelector("input")
-let ammount=1
-let amm=Number(ammount)
-let frm
-let rateAmount
-getData()
-let btn= async ()=>{
-    button.addEventListener("click", async(evt)=>{
-        evt.preventDefault();
-        ammount=data.value
-        amm =Number(ammount)
-        if(amm==="" || amm<=0){
-            amm=1
-            data.value=1
-        }
-        data.value=null
-        getRate =all[1].value
-        fro=all[0].value
-        val =document.querySelector(".value")
-        n = `${url}/${all[0].value}`
-        let da =  await fetch(n)
-        respo = await da.json()
-        frm = respo.rates[from]
-        rateAmount=respo.rates[getRate]
-        val.innerText=`${amm} ${fro} IN ${getRate} = ${rateAmount*amm}`
-    })
-}
-btn()
+
+// 🔹 Button click
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getRate();
+});
+
+// 🔹 Swap currencies (↔)
+exchange.addEventListener("click", async () => {
+    
+    let temp = selects[0].value;
+    selects[0].value = selects[1].value;
+    selects[1].value = temp;
+
+
+    updateFlag(selects[0]);
+    updateFlag(selects[1]);
+
+    getRate();
+});
+
+
+getRate();
